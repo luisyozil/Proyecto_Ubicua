043703a4cd9a0c513957ccd92c4ed16c36e94540
LuisPabloRocha <luispablo2098@hotmail.com>
2020-05-15 16:50:20 -0500

    Cambios Paquetes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:proyecto_ubicua/Paquetes.dart';
 import 'package:proyecto_ubicua/login2.dart';
+import 'package:proyecto_ubicua/paquetes2.dart';
 import 'package:proyecto_ubicua/prueba.dart';
 import 'Login.dart';
 import 'intro.dart';
@@ -7,14 +9,15 @@ import 'intro.dart';
 void main() => runApp(
   MaterialApp(
     themeMode: ThemeMode.dark,
-    theme: ThemeData(fontFamily: 'Proxima_nova',
+
+    theme: ThemeData(fontFamily: 'Ubuntu',
         primaryColor: Colors.grey[900],
         accentColor: Colors.yellow[600],
         cursorColor: Colors.yellow[600],
         brightness: Brightness.dark,
-        scaffoldBackgroundColor: Colors.black
+        scaffoldBackgroundColor: Colors.black,
     ),
     debugShowCheckedModeBanner: false,
-    home: IntroPage(),
+    home: Paquetes2(),
   ),
 );
