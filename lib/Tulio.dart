5530ba05b02610530122218f611a8535afd94017
nunezmarco <subodai1@gmail.com>
2020-05-22 15:04:18 -0500

    Pagos con tarjeta

--- lib/Tulio.dart
+++ lib/Tulio.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'db.dart' as db;
 import 'modelos/Usuario.dart';
+import 'package:stripe_payment/stripe_payment.dart';
+import 'package:progress_dialog/progress_dialog.dart';
+import 'services/payment-service.dart';
 
 class perfil extends StatelessWidget {
   Widget build(BuildContext context) {
@@ -964,5 +967,97 @@ class perfil2 extends StatelessWidget {
         );
       },
     );
+  }
+}
+
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+
+  onItemPress(BuildContext context, int index) async {
+    switch(index) {
+      case 0:
+        payViaNewCard(context);
+        break;
+      case 1:
+        Navigator.pushNamed(context, 'lib/pages/existing-cards.dart');
+        break;
+    }
+  }
+
+  payViaNewCard(BuildContext context) async {
+    ProgressDialog dialog = new ProgressDialog(context);
+    dialog.style(
+        message: 'Por favor espera...'
+    );
+    await dialog.show();
+    var response = await StripeService.payWithNewCard(
+        amount: '15000',
+        currency: 'USD'
+
+    );
+    await dialog.hide();
+    Scaffold.of(context).showSnackBar(
+        SnackBar(
+          content: Text(response.message),
+          duration: new Duration(milliseconds: response.success == true ? 1200 : 3000),
+        )
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    StripeService.init();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: ListView.separated(
+            itemBuilder: (context, index) {
+              Icon icon;
+              Text text;
+
+              switch(index) {
+                case 0:
+                  icon = Icon(Icons.add_circle, color: theme.primaryColor);
+                  text = Text('Añadir una tarjeta nueva');
+                  break;
+                case 1:
+                  icon = Icon(Icons.credit_card, color: theme.primaryColor);
+                  text = Text('Pagar con una tarjeta existente');
+                  break;
+              }
+
+              return InkWell(
+                onTap: () {
+                  onItemPress(context, index);
+                },
+                child: ListTile(
+                  title: text,
+                  leading: icon,
+                ),
+              );
+            },
+            separatorBuilder: (context, index) => Divider(
+              color: theme.primaryColor,
+            ),
+            itemCount: 2
+        ),
+      ),
+    );;
   }
 }
